8fd3a0f6ed90d2da4cf49cc64552a14b13eba93c
Eun12341234 <shinje9511@naver.com>
2024-03-27 17:25:13 +0900

    html&css수정

--- src/main/java/hello/group/controller/Controller.java
+++ src/main/java/hello/group/controller/Controller.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller { // 메인 컨트롤러
 
+    @GetMapping("/main")
+    public String mainPage(){
+        return"/main";
+    }
+
     @GetMapping("/logIn") // 로그인 페이지 매핑
     public String logInPage(){
         return "/login";
@@ -18,6 +23,16 @@ public class Controller { // 메인 컨트롤러
     @GetMapping("/signUp") // 회원가입 페이지 매핑
     public String signUpPage(){
         return "/signup";
+    }
+
+    @GetMapping("result")
+    public String a(){
+        return "/result";
+    }
+
+    @GetMapping("imformation")
+    public String imformation(){
+        return "/editimformation";
     }
 
 }
---
+++ src/main/resources/static/js/click.js
@@ -0,0 +1,6 @@
+function btnClick() {
+    const myinput
+        = document.getElementById('my-input');
+
+    myinput.value = "안녕하세요";
+}
